239baf9c467b7e3f3e4f520d65e2e19f9a85935a
LanceBongon <lance.bongon@outlook.com>
2024-08-25 16:15:18 +1000

    Made the class generic

--- assets/UIHandler.js
+++ assets/UIHandler.js
@@ -1,13 +1,5 @@
-//This class is responsible for the buttons, input, and document refference.
-//This class is attached to the HTML and it has a lot of coupling to HTML.
-//Without it the HTML will not be interactable.
-//When I was making a front-end project that lives in the browser and is only a local application. 
-//I found myself in a messy situation where I spend more time finding where I placed that piece of code is. 
-//Rather than actually fixing the code, if you get what I mean.
-
- //Global referrence to the initiated.
- class UIHandler {
-    static #docRef = {};  // Static private field shared across all instances
+class UIHandler {
+    static docRef = {};  // Static field shared across all instances
 
     constructor(elementsName = []) {
         this.#initialiseDocRef(elementsName);
@@ -24,26 +16,25 @@
     #addDocRef(elementId) {
         const element = document.getElementById(elementId);
         if (element) {
-            this.docRef[elementId] = element;
+            this.constructor.docRef[elementId] = element;
             console.log(`Element with ID '${elementId}' has been added.`);
         } else {
             console.warn(`Element with ID '${elementId}' not found.`);
         }
     }
 
-    // Getter to retrieve the whole #docRef object
+    // Getter to retrieve the whole static docRef object
     get docRef() {
-        return UIHandler.#docRef;
+        return this.constructor.docRef;
     }
 
-    // Setter to remove an element by its ID from #docRef
+    // Setter to remove an element by its ID from static docRef
     set removeDocRef(elementId) {
-        if (elementId in UIHandler.#docRef) {
-            delete UIHandler.#docRef[elementId];
+        if (elementId in this.constructor.docRef) {
+            delete this.constructor.docRef[elementId];
             console.log(`Element with ID '${elementId}' has been removed.`);
         } else {
             console.warn(`Element with ID '${elementId}' does not exist in docRef.`);
         }
     }
 }
-
