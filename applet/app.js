498694f1745a8703a2987032e7dc23f262496a56
LanceBBongon <lance.bongon@outlook.com>
2025-07-10 01:30:24 +1000

    Re organised files...

---
+++ applet/app.js
@@ -0,0 +1,17 @@
+//This folder is where a lot of global stuff are stored used all through out the application.
+
+
+class TodoApp {
+    constructor() {
+        console.log("To-do app loaded")
+        this.initialise();
+    }
+
+    initialise() {
+        
+    }
+}
+
+
+
+const app = new TodoApp();
